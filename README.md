836a4b7c96ff25dbeb3a04f1ca97c5934e555bd4
JavierGalliano <javiergalliano1@gmail.com>
2022-07-20 18:04:42 -0300

    Desafio CartView

---
+++ README.md
@@ -0,0 +1,3 @@
+nombre:mi tienda online
+autor: Javier galliano
+descripción: mi tienda online es un proyecto basado en react en el que se busca simular una tienda online.
---
+++ src/components/NavBar.js
@@ -0,0 +1,33 @@
+import { useState, useEffect } from 'react'
+import './NavBar.css'
+import logo from '../imagenes/logoTienda.png'
+import CartWidget from './CartWidget'
+import { NavLink } from 'react-router-dom'
+
+const NavBar = () =>{
+
+    const [categorias, setCategorias] = useState([])
+    useEffect(()=>{
+        setTimeout(()=>{
+        fetch('https://fakestoreapi.com/products/categories')
+        .then(res=>res.json())
+        .then(json=>{
+            setCategorias(json)
+        })
+        .catch(err=>console.log(err))
+        }, 500)
+    },[])
+
+    return(
+    <header>
+        <NavLink to="/"><img src={logo} alt="logo"/></NavLink>
+        <NavLink to="/"><h1>Tienda<br/>Online</h1></NavLink>
+        <nav>
+            {categorias.map((categorias)=><NavLink key={categorias} to={`/category/${categorias}`}>{categorias}</NavLink>)}
+        </nav>
+        <CartWidget/>
+    </header>
+    )
+}
+
+export default NavBar
