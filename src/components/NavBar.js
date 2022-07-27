9cdd4a71f3aed3c9c3f83e2dd12559cbda8ec68a
JavierGalliano <javiergalliano1@gmail.com>
2022-07-27 17:31:37 -0300

    Firebase2

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,11 +2,79 @@ import { useState, useEffect } from 'react'
 import './NavBar.css'
 import logo from '../imagenes/logoTienda.png'
 import CartWidget from './CartWidget'
-import { NavLink } from 'react-router-dom'
-import {db} from '../firebase/firebase'
-import { getDoc, collection, doc } from 'firebase/firestore'
+import { Link, NavLink } from 'react-router-dom'
 
-const NavBar = () =>{
+const Navbar = () => {
+    const viewport = {
+        width: document.documentElement.clientWidth,
+        height: document.documentElement.clientHeight
+    }
+
+    const categories = [
+        { name: "electronics", id: 0, route: "/category/electronics" },
+        { name: "jewelery", id: 1, route: "/category/jewelery" },
+        { name: "men's clothing", id: 2, route: "/category/men's clothing" },
+        { name: "women's clothing", id: 3, route: "/category/women's clothing" },
+    ];
+
+    return (
+        <header style={styles.container}>
+            <div style={styles.branchContainer}>
+                <Link to="/"><img style={styles.logo} src={logo} alt="" /></Link>
+                <h1 style={styles.title}>Mi tienda online</h1>
+            </div>
+            <div style={styles.links}>
+                <nav>
+                    {categories.map((category) => <NavLink key={category.id} style={styles.link} to={category.route}>{category.name}</NavLink>)}
+                </nav>
+                <Link to="/cart"><CartWidget /></Link>
+            </div>
+        </header >
+    )
+}
+const styles = {
+    container: {
+        display: 'flex none',
+        
+        justifyContent: "space-between",
+        alignItems: "center",
+        width: "100%"
+    },
+    branchContainer: {
+        display: "flex",
+        justifyContent: "flex-start",
+        alignItems: "center"
+    },
+    title: {
+        fontSize: 25
+    },
+    links: {
+        display: "flex"
+    },
+    link: {
+        fontSize: 16,
+        padding: 20
+    },
+    logo: {
+        height: 80
+    }
+};
+
+export default Navbar 
+
+
+
+
+
+
+
+
+
+
+/* import {db} from '../firebase/firebase'
+import { getDoc, collection, doc } from 'firebase/firestore' */
+
+/* const NavBar = () =>{
 
     const [categorias, setCategorias] = useState([])
     useEffect(()=>{
@@ -33,4 +101,4 @@ const NavBar = () =>{
     )
 }
 
-export default NavBar
+export default NavBar */
